6e1b8a433b0f982ed4acc8d428f1dc6f96143eed
Joao Stein <joaosteing@gmail.com>
2011-05-22 16:39:38 -0300

    Destaque da home com carousel no paginador

--- js/script.js
+++ js/script.js
@@ -2,14 +2,33 @@
 	Joao Stein [http://www.joaostein.com.br + http://www.hostdesk.com.br]
 */
 
+$(function(){
+	$('#destaque').cycle({
+		fx: 'fade',
+		speed:    1200, 
+		timeout:  8000,
+		pager:  '.pager',
+		pagerAnchorBuilder: criarPaginador,
+		pause: true
+	});
 
-$('#destaque').cycle({
-	fx: 'fade',
-	speed:    1200, 
-	timeout:  8000,
-	pager:  '.pager',
-	pause: true,
+function criarPaginador(id, slide) {
+		var idFinal = id + 1;
+		
+		var imgThumb = $(slide).attr('data-urlThumb');
+		
+		return '<li><a href="#"><img src="' + imgThumb + '" alt="' + slide.title + '" title="'+ slide.title +'" width="180" height="100"></a></li>';
+}
+
+$('#carousel').jcarousel();
+
+	
 });
+
+
+
+
+
 
 
 
