c27eac23aec99c0334480f4085ecf4dcfb085b81
Joao Stein <joaosteing@gmail.com>
2011-05-26 16:21:45 -0300

    Gerada estrutura p/ redes sociais no footer + efeito da nuvem se movendo + lógica para funcionamento do carousel no destaque

--- js/script.js
+++ js/script.js
@@ -8,21 +8,52 @@ $(function(){
 	$('#destaque').cycle({
 		fx: 'fade',
 		speed:    1200, 
-		timeout:  8000,
+		timeout:  1000,
 		pager:  '.pager',
 		pagerAnchorBuilder: criarPaginador,
-		pause: true
+		before: verificarAnimacao,
+		pause: true,
+		pagerEvent: 'mouseover'
 	});
+	
+$('#carousel').jcarousel({
+	initCallback: giveCarousel,
+	scroll: 4
+});
 
 function criarPaginador(id, slide) {
-		var idFinal = id + 1;
-		
-		var imgThumb = $(slide).attr('data-urlThumb');
-		
-		return '<li><a href="#"><img src="' + imgThumb + '" alt="' + slide.title + '" title="'+ slide.title +'" width="180" height="100"></a></li>';
+	var idFinal = id + 1;
+	var imgThumb = $(slide).attr('data-urlThumb');
+	return '<li><a href="#"><img src="' + imgThumb + '" alt="' + slide.title + '" title="'+ slide.title +'" width="180" height="100"></a></li>';
 }
 
-$('#carousel').jcarousel();
+function giveCarousel(carousel) {
+	carouselObject = carousel;
+}
+
+function verificarAnimacao(currSlideElement, nextSlideElement, options, forwardFlag) {
+	posicaoAtual = options.nextSlide + 1;
+	changePosition();
+}
+
+function changePosition() {
+	if( posicaoAtual % 5 == 0 || posicaoAtual == 1) {
+		carouselObject.scroll(posicaoAtual)
+	}
+}
+
+$('.jcarousel-skin-tango').hover(pagerOverPause, pagerOutResume);
+
+function pagerOverPause(){
+	$('#destaque').cycle('pause');
+	$('#destaque').toggleClass('hovered');
+}
+
+function pagerOutResume(){
+	$('#destaque').cycle('resume');
+	$('#destaque').toggleClass('hovered');
+}
+
 
 });
 
@@ -49,6 +80,11 @@ $(function(){
 		pause: true
 	});
 });
+
+/*---- Efeito de animação da nuvem ----*/
+
+$('html').pan({fps: 15, speed: 2, dir: 'left'});
+
 
 
 
